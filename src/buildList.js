dc0c5ceff27b931cfac7b6b81e1709bd07f0fc20
Vasu N <vasun77@gmail.com>
2021-11-22 21:29:30 +0530

    refine buildList method

--- src/buildList.js
+++ src/buildList.js
@@ -1,20 +1,6 @@
-import { fragment } from "xmlbuilder2";
 import VNode from "virtual-dom/vnode/vnode";
-import VText from "virtual-dom/vnode/vtext";
 import isVNode from "virtual-dom/vnode/is-vnode";
 import isVText from "virtual-dom/vnode/is-vtext";
-import escape from 'escape-html';
-import * as xmlBuilder from './htmlToXmlBuilder';
-import namespaces from "./xmlNamespaces";
-import { buildList, createNumbering } from "./buildList";
-
-
-//const HTMLToVDOM = HTMLToVDOM_;
-const getTreeFromHTML = require('html-to-vdom')({
-  VNode,
-  VText,
-});
-/*
 
 const createNumberingNode = (listElements, id) => {
   let arr = [];
@@ -39,6 +25,7 @@ const createNumberingNode = (listElements, id) => {
   listElements.filter((value, index, self) => {
     return self.findIndex((v) => v.level === value.level) === index;
   }).forEach(({level, type}) => {
+    //console.log(`-----------level = ${level} and type is ${type}`);
     let lvl = {
       _parent: ab,
       _children: [],
@@ -74,7 +61,7 @@ const createNumberingNode = (listElements, id) => {
       _children:[],
       _fTextNode: false,
       _attrs:{
-        //'w:val': type === 'ol' ? `%${level + 1}` : '',
+        /*'w:val': type === 'ol' ? `%${level + 1}` : '',*/
         'w:val': type === 'ol' ? `%${level + 1}` : '\u2022',
       },
       _tag: 'w:lvlText'
@@ -167,6 +154,7 @@ const createNumbering = (listElements, prepped_secondaries) => {
     let filename = prepped_secondaries[i][len]
     if (filename === 'word/numbering.xml') {
       numbering = prepped_secondaries[i];
+      //console.log(`checking secondary numbering file`, prepped_secondaries[i][0]);
       if (prepped_secondaries[i][0]._children.length) {
         numberEl = prepped_secondaries[i][0]._children;
         for (let k = 0; k < prepped_secondaries[i][0]._children.length; k++) {
@@ -200,7 +188,6 @@ const createNumbering = (listElements, prepped_secondaries) => {
   let listNodes = createNumberingNode(listElements, numberId);
   listNodes["_parent"] = parent;
   numberEl.splice(numberId, 0, listNodes);
-  console.log('listNodes => ', listNodes);
   let numNode = {
     _parent: parent,
     _children: [],
@@ -301,126 +288,4 @@ const buildList = (vNode) => {
   return listElements;
 };
 
-*/
-function findXMLEquivalent(vNode, xmlFragment, prepped_secondaries) {
-  if (
-    vNode.tagName === 'div' &&
-    (vNode.properties.attributes.class === 'page-break' ||
-      (vNode.properties.style && vNode.properties.style['page-break-after']))
-  ) {
-    const paragraphFragment = fragment({
-      namespaceAlias: { w: namespaces.w },
-    })
-      .ele('@w', 'p')
-      .ele('@w', 'r')
-      .ele('@w', 'br')
-      .att('@w', 'type', 'page')
-      .up()
-      .up()
-      .up();
-
-    xmlFragment.import(paragraphFragment);
-    return;
-  }
-
-  switch (vNode.tagName) {
-    case 'h1':
-    case 'h2':
-    case 'h3':
-    case 'h4':
-    case 'h5':
-    case 'h6':
-      const headingFragment = xmlBuilder.buildParagraph(
-        vNode,
-        {
-          paragraphStyle: `Heading${vNode.tagName[1]}`,
-        },
-      );
-      xmlFragment.import(headingFragment);
-      return;
-    case 'span':
-    case 'strong':
-    case 'b':
-    case 'em':
-    case 'i':
-    case 'u':
-    case 'ins':
-    case 'strike':
-    case 'del':
-    case 's':
-    case 'sub':
-    case 'sup':
-    case 'mark':
-    case 'p':
-    case 'a':
-    case 'blockquote':
-    case 'code':
-    case 'pre':
-      const paragraphFragment = xmlBuilder.buildParagraph(vNode, {}, prepped_secondaries);
-      xmlFragment.import(paragraphFragment);
-      return;
-    case 'ol':
-    case 'ul':
-      const listElements = buildList(vNode);
-      const numberingId = createNumbering(listElements, prepped_secondaries);
-      // eslint-disable-next-line no-plusplus
-      for (let index = 0; index < listElements.length; index++) {
-        const listElement = listElements[index];
-        // eslint-disable-next-line no-shadow
-        const paragraphFragment = xmlBuilder.buildParagraph(
-          listElement.node,
-          {
-            numbering: { levelId: listElement.level, numberingId },
-          }
-        );
-        xmlFragment.import(paragraphFragment);
-      }
-      return;
-    case 'br':
-      const linebreakFragment = xmlBuilder.buildParagraph(null, {});
-      xmlFragment.import(linebreakFragment);
-      return;
-    default:
-      break;
-  }
-  if (vNode.children && Array.isArray(vNode.children) && vNode.children.length) {
-    // eslint-disable-next-line no-plusplus
-    for (let index = 0; index < vNode.children.length; index++) {
-      const childVNode = vNode.children[index];
-      // eslint-disable-next-line no-use-before-define
-      convertVTreeToXML(childVNode, xmlFragment);
-    }
-  }
-}
-
-// eslint-disable-next-line consistent-return
-export function convertVTreeToXML(vTree, xmlFragment, prepped_secondaries) {
-  if (!vTree) {
-    // eslint-disable-next-line no-useless-return
-    return '';
-  }
-  if (Array.isArray(vTree) && vTree.length) {
-    // eslint-disable-next-line no-plusplus
-    for (let index = 0; index < vTree.length; index++) {
-      const vNode = vTree[index];
-      convertVTreeToXML(vNode, xmlFragment);
-    }
-  } else if (isVNode(vTree)) {
-    findXMLEquivalent(vTree, xmlFragment, prepped_secondaries);
-  } else if (isVText(vTree)) {
-    xmlBuilder.buildTextElement(xmlFragment, escape(String(vTree.text)));
-  }
-  return xmlFragment;
-}
-
-const convertHtml = (htmlStr, prepped_secondaries) =>  {
-  const vTree = getTreeFromHTML(htmlStr);
-  const xmlFragment = fragment({
-    namespaceAlias: {w: namespaces.w},
-  });
-
-  const populatedXmlFragment = convertVTreeToXML(vTree, xmlFragment, prepped_secondaries);
-  return populatedXmlFragment.toObject();
-}
-
-export default convertHtml;
+export { buildList, createNumbering };
